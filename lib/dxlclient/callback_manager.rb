295ab17cdd634c69682b09ed9a254b7bd106239d
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-11-17 12:03:01 -0800

    Callback manager refactor for rubocop code complexity

--- lib/dxlclient/callback_manager.rb
+++ lib/dxlclient/callback_manager.rb
@@ -21,19 +21,8 @@ module DXLClient
       end
 
       @client.subscribe(topic) if subscribe_to_topic
-
-      callbacks_by_topic = @callbacks_by_class[klass]
-      unless callbacks_by_topic
-        callbacks_by_topic = {}
-        @callbacks_by_class[klass] = callbacks_by_topic
-      end
-
-      callbacks = callbacks_by_topic[topic]
-      unless callbacks
-        callbacks = Set.new
-        callbacks_by_topic[topic] = callbacks
-      end
-
+      callbacks_by_topic = callbacks_for_class!(klass)
+      callbacks = callbacks_for_topic!(callbacks_by_topic, topic)
       callbacks.add(CallbackInfo.new(callback, subscribe_to_topic))
     end
 
@@ -45,44 +34,22 @@ module DXLClient
       entry = callbacks.find do |callback_info|
         callback_info.callback == callback
       end
-      if callbacks.delete?(entry) && unsubscribe &&
-         !topic_subscribed?(topic)
-        @client.unsubscribe(topic)
-      end
+      return if !callbacks.delete?(entry) || !unsubscribe ||
+                topic_subscribed?(topic)
+      @client.unsubscribe(topic)
     end
 
     # @param message [DXLClient::Message]
     def on_message(message)
       @logger.debugf('Received message. Type: %s. Id: %s.',
                      message.class.name, message.message_id)
-      class_callbacks = @callbacks_by_class[message.class] ||
-                        @callbacks_by_class[
-                          @callbacks_by_class.keys.find do |klass|
-                            message.is_a?(klass)
-                          end]
-
-      if class_callbacks
-        matching_class_callbacks = class_callbacks.select do |topic|
-          (topic == message.destination_topic) ||
-            (topic[-1] == '#' &&
-              message.destination_topic.start_with?(topic[0...-1]))
-        end
-        if matching_class_callbacks.length.zero?
-          @logger.debugf('No callbacks registered for topic: %s. Id: %s.',
-                         message.destination_topic, message.message_id)
-        else
-          matching_class_callbacks.each_value do |topic_callbacks|
-            topic_callbacks.map(&:callback).each do |callback|
-              @callback_thread_pool.add_task do
-                message.invoke_callback(callback)
-              end
-            end
-          end
-        end
-      else
-        @logger.debugf('No callbacks registered for message type: %s. Id: %s.',
-                       message.class.name, message.message_id)
+      class_callbacks = callbacks_for_message(message)
+      matching_callbacks = class_callbacks.select do |topic|
+        (topic == message.destination_topic) ||
+          (topic[-1] == '#' &&
+            message.destination_topic.start_with?(topic[0...-1]))
       end
+      invoke_callbacks_for_class!(matching_callbacks, message)
     end
 
     def destroy
@@ -91,19 +58,48 @@ module DXLClient
 
     private
 
-    def topics_to_subscribe
-      @callbacks_by_class.values.reduce(Set.new) do |topics, callbacks_by_topic|
-        topics_for_klass =
-          callbacks_by_topic.reduce(
-            Set.new
-          ) do |klass_topics, (topic, callbacks)|
-            if callbacks.any?(&:subscribe?)
-              klass_topics.add(topic)
-            else
-              klass_topics
-            end
-          end
-        topics.merge(topics_for_klass)
+    def callbacks_for_message(message)
+      @callbacks_by_class[message.class] ||
+        @callbacks_by_class[
+          @callbacks_by_class.keys.find do |klass|
+            message.is_a?(klass)
+          end] || {}
+    end
+
+    def callbacks_for_class!(klass)
+      callbacks_by_topic = @callbacks_by_class[klass]
+      unless callbacks_by_topic
+        callbacks_by_topic = {}
+        @callbacks_by_class[klass] = callbacks_by_topic
+      end
+      callbacks_by_topic
+    end
+
+    def callbacks_for_topic!(callbacks_by_topic, topic)
+      callbacks = callbacks_by_topic[topic]
+      unless callbacks
+        callbacks = Set.new
+        callbacks_by_topic[topic] = callbacks
+      end
+      callbacks
+    end
+
+    def invoke_callbacks_for_class!(callbacks, message)
+      if callbacks.length.zero?
+        @logger.debugf('No callbacks registered for topic: %s. Id: %s.',
+                       message.destination_topic, message.message_id)
+      else
+        callbacks.each_value do |topic_callbacks|
+          invoke_callbacks_for_topic!(topic_callbacks, message)
+        end
+      end
+    end
+
+    def invoke_callbacks_for_topic!(callbacks, message)
+      callbacks.map(&:callback).each do |callback|
+        @callback_thread_pool.add_task do
+          message.invoke_callback(callback)
+        end
       end
     end
 
