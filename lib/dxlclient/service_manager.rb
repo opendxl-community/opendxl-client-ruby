c067a49c095cb5da8b895431d64b0f3dd64a098a
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-11-20 17:25:59 -0800

    Service manager refactor for rubocop code complexity

--- lib/dxlclient/service_manager.rb
+++ lib/dxlclient/service_manager.rb
@@ -41,13 +41,7 @@ module DXLClient
 
     def destroy
       @logger.debug('Destroying service manager...')
-      services_to_destroy = nil
-      @services_lock.synchronize do
-        services_to_destroy = @services.values
-      end
-      services_to_destroy.each do |service_reg_info|
-        remove_service_sync(service_reg_info)
-      end
+      remove_all_registered_services
       @services_ttl_loop_continue = false
       @services_ttl_condition.broadcast
       @services_ttl_thread.join
@@ -91,14 +85,7 @@ module DXLClient
     # @param service_reg_info [DXLClient::ServiceRegistrationInfo]
     def remove_service_sync(service_reg_info,
                             timeout = SERVICE_UNREGISTRATION_REQUEST_TIMEOUT)
-      request = unregister_service_request(service_reg_info)
-      response = @client.sync_request(request, timeout)
-      if response.message_type == DXLClient::Message::MESSAGE_TYPE_ERROR
-        raise DXLClient::DXLError,
-              format('Error unregistering service %s: %s. Code: %s',
-                     service_reg_info.service_type, res.error_message,
-                     res.error_code)
-      end
+      unregister_service_sync(service_reg_info, timeout)
       remove_service_callbacks(service_reg_info)
       @services_lock.synchronize do
         @services.delete(service_reg_info.service_id)
@@ -107,19 +94,11 @@ module DXLClient
 
     def on_connect
       @services_lock.synchronize do
-        connected = true
-        @services.each_value do |service|
-          begin
-            register_service_sync(service)
-          rescue MQTT::NotConnectedException
-            @logger.errorf(
-              'Error registering service %s, client disconnected',
-              service.service_type
-            )
-            connected = false
-          end
+        all_services_registered = @services.values.all? do |service|
+          @logger.debug("Re-registering service: #{service.service_type}")
+          register_service_if_connected(service)
         end
-        @services_ttl_condition.broadcast if connected
+        @services_ttl_condition.broadcast if all_services_registered
       end
     end
 
@@ -129,92 +108,114 @@ module DXLClient
       @logger.debug('Services ttl monitor thread started')
 
       @services_lock.synchronize do
-        while @services_ttl_loop_continue
-          if @services.empty?
-            @services_ttl_condition.wait(@services_lock)
-          else
-            connected = true
-            min_service_ttl =
-              @services.values.reduce(nil) do |min_so_far, service|
-                service_ttl_seconds = service.ttl * 60
-                if service.last_registration.nil?
-                  ttl_remaining = 0
-                else
-                  now = Time.now
-                  if now < service.last_registration
-                    service.last_registration = now
-                  end
-                  ttl_remaining = service.last_registration - now +
-                                  service_ttl_seconds
-                end
-                if ttl_remaining <= 0
-                  begin
-                    register_service_sync(service)
-                  rescue MQTT::NotConnectedException
-                    @logger.errorf(
-                      'Error registering service %s, client disconnected',
-                      service.service_type
-                    )
-                    connected = false
-                    break
-                  end
-                  ttl_remaining = service_ttl_seconds
-                end
-                if min_so_far.nil? || ttl_remaining < min_so_far
-                  ttl_remaining
-                else
-                  min_so_far
-                end
-              end
-            if connected
-              wait_time = min_service_ttl < 5 ? 5 : min_service_ttl
-              @services_ttl_condition.wait(
-                @services_lock,
-                wait_time < 5 ? 5 : wait_time
-              )
-            else
-              @services_ttl_condition.wait(@services_lock)
-            end
-          end
-        end
+        process_service_ttls while @services_ttl_loop_continue
       end
 
       @logger.debug('Services ttl monitor thread terminating')
     end
 
-    # @param service_reg_info [DXLClient::ServiceRegistrationInfo]
-    def register_service_request(service_reg_info)
+    def process_service_ttls
+      min_service_ttl = minimum_service_ttl
+      if min_service_ttl
+        if min_service_ttl > 0
+          @services_ttl_condition.wait(@services_lock, min_service_ttl)
+        end
+      else
+        @services_ttl_condition.wait(@services_lock)
+      end
+    end
+
+    def minimum_service_ttl
+      @services.values.reduce(nil) do |min_so_far, service|
+        min_so_far = compare_service_ttl(min_so_far, service)
+        break if min_so_far == false
+        min_so_far
+      end
+    end
+
+    # @param service [DXLClient::ServiceRegistrationInfo]
+    def compare_service_ttl(min_so_far, service)
+      ttl_remaining = reregister_service_if_ttl_expired(
+        service_ttl_remaining(service),
+        service
+      )
+      if min_so_far.nil? || !ttl_remaining == false ||
+         ttl_remaining < min_so_far
+        ttl_remaining
+      else
+        min_so_far
+      end
+    end
+
+    def reregister_service_if_ttl_expired(ttl_remaining, service)
+      if ttl_remaining <= 0
+        @logger.debug(
+          "TTL expired, re-registering service: #{service.service_type}"
+        )
+        ttl_remaining = service.ttl if register_service_if_connected(service)
+      end
+      ttl_remaining
+    end
+
+    def service_ttl_remaining(service)
+      service_ttl_seconds = service.ttl * 60
+      if service.last_registration.nil?
+        0
+      else
+        now = Time.now
+        service.last_registration = now if now < service.last_registration
+        service.last_registration - now + service_ttl_seconds
+      end
+    end
+
+    # @param service [DXLClient::ServiceRegistrationInfo]
+    def register_service_request(service)
       request = DXLClient::Request.new(DXL_SERVICE_REGISTER_REQUEST_TOPIC)
-      request.payload = JSON.dump(serviceType: service_reg_info.service_type,
-                                  metaData: service_reg_info.metadata,
-                                  requestChannels: service_reg_info.topics,
-                                  ttlMins: service_reg_info.ttl,
-                                  serviceGuid: service_reg_info.service_id)
+      request.payload = JSON.dump(serviceType: service.service_type,
+                                  metaData: service.metadata,
+                                  requestChannels: service.topics,
+                                  ttlMins: service.ttl,
+                                  serviceGuid: service.service_id)
       request.destination_tenant_guids =
-        service_reg_info.destination_tenant_guids
+        service.destination_tenant_guids
       request
     end
 
-    def register_service_sync(service_reg_info)
-      request = register_service_request(service_reg_info)
+    def register_service_if_connected(service)
+      register_service(service)
+      true
+    rescue MQTT::NotConnectedException
+      @logger.errorf(
+        'Error registering service %s, client disconnected',
+        service.service_type
+      )
+      false
+    end
+
+    def register_service(service)
+      request = register_service_request(service)
       response = @client.sync_request(request)
-      if response.message_type == DXLClient::Message::MESSAGE_TYPE_ERROR
-        @logger.errorf(
-          'Error registering service %s: %s. Code: %s.',
-          service_reg_info.service_type, response.error_message,
-          response.error_code
-        )
-        false
-      else
-        service_reg_info.last_registration = Time.now
-        true
-      end
+      handle_register_service_response(service, response)
     rescue StandardError => e
       @logger.errorf(
         'Error registering service %s: %s. Code: %s.',
-        service_reg_info.service_type, e.class.name, e.message
+        service.service_type, e.class.name, e.message
       )
       false
+    end
+
+    # @param service [DXLClient::ServiceRegistrationInfo]
+    def handle_register_service_response(service, response)
+      if response.message_type == DXLClient::Message::MESSAGE_TYPE_ERROR
+        @logger.errorf(
+          'Error registering service %s: %s. Code: %s.',
+          service.service_type, response.error_message, response.error_code
+        )
+        false
+      else
+        service.last_registration = Time.now
+        true
+      end
     end
 
     # @param service_reg_info [DXLClient::ServiceRegistrationInfo]
@@ -235,6 +236,19 @@ module DXLClient
       end
     end
 
+    def unregister_service_sync(service_reg_info, timeout)
+      request = unregister_service_request(service_reg_info)
+      response = @client.sync_request(request, timeout)
+      # rubocop: disable GuardClause
+      if response.message_type == DXLClient::Message::MESSAGE_TYPE_ERROR
+        error = format('Error unregistering service %s: %s. Code: %s',
+                       service_reg_info.service_type, response.error_message,
+                       response.error_code)
+        raise DXLClient::DXLError, error
+      end
+      # rubocop: enable GuardClause
+    end
+
     # @param service_reg_info [DXLClient::ServiceRegistrationInfo]
     def unregister_service_request(service_reg_info)
       request = DXLClient::Request.new(DXL_SERVICE_UNREGISTER_REQUEST_TOPIC)
@@ -248,6 +262,16 @@ module DXLClient
         service_reg_info.callbacks(topic).each do |callback|
           @client.remove_request_callback(topic, callback)
         end
+      end
+    end
+
+    def remove_all_registered_services
+      services_to_destroy = nil
+      @services_lock.synchronize do
+        services_to_destroy = @services.values
+      end
+      services_to_destroy.each do |service_reg_info|
+        remove_service_sync(service_reg_info)
       end
     end
   end
